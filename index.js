acd38cbb71236d037c28d544cf9c7a4726753df5
Balna <balna001@gold.ac.uk>
2025-10-16 17:38:50 +0100

    initial version

---
+++ index.js
@@ -0,0 +1,13 @@
+var http = require("http"); 
+const fs = require("fs");
+const port = 8000; 
+
+
+http.createServer(function(req, res) { 
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const html = fs.readFileSync("index.html");
+    res.end(html);
+    }).listen(port, function() { 
+        console.log(`Node server is running on port ${port}...`); 
+}); 
+
